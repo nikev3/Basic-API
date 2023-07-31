ab0fe2b009042836003c1ed9056b17d59b84f770
Nikolai Evans <122506274+nikev3@users.noreply.github.com>
2023-07-31 12:52:20 -0700

    Update movie-api.js

--- movie-api.js
+++ movie-api.js
@@ -1,6 +1,6 @@
-
+const passwrd='cfastudentuser'
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://cfastudentuser:<password>@cluster0.nfsnbwr.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://cfastudentuser:"+passwrd+"@cluster0.nfsnbwr.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,19 +11,8 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
 
 
 const express = require('express');
@@ -43,9 +32,23 @@ const movieTitles = [
   'Cats & Dogs',
 ];
 
-app.get('/all', (req, res) => {
+app.get('/all', async (req, res) => { // <-- Mark the callback function as async
+
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+
   res.json(movieTitles);
+
 });
+
 
 app.get('/find', (req, res) => {
   const searchTerm = req.query.contains || req.query.startsWith;
