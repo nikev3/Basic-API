9c109d63c9f8eb8d3b4a0e1d5523cac78c7c7021
Nikolai Evans <122506274+nikev3@users.noreply.github.com>
2023-07-31 13:10:14 -0700

    Update movie-api.js

--- movie-api.js
+++ movie-api.js
@@ -1,7 +1,13 @@
-const passwrd='cfastudentuser'
+const express = require('express')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://cfastudentuser:"+passwrd+"@cluster0.nfsnbwr.mongodb.net/?retryWrites=true&w=majority";
+const PORT = 3000
 
+const app = express()
+app.use(cors())
+
+
+const uri = "";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -11,64 +17,64 @@ const client = new MongoClient(uri, {
   }
 });
 
-
-
-
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 3000; // You can use any desired port number
-
-app.use(cors());
-
-const movieTitles = [
-  'The Avengers',
-  'All Dogs Go to Heaven',
-  'The Aristocats',
-  'The Brave Little Toaster',
-  'The Lord of the Rings',
-  'The Revenant',
-  'Cats & Dogs',
+let movieList = [
+  'Themm Avengers', 
+  'All Dogs Go To Heaven', 
+  'The Aristocats', 
+  'The Brave Little Toaster', 
+  'The Lord of the Rings', 
+  'The Revenant', 
+  'Cats & Dogs'
 ];
 
-app.get('/all', async (req, res) => { // <-- Mark the callback function as async
+app.route('/all')
+  .get(async (req, res) => {
+    let result = [];
+    let error = null;
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      const collection = client.db("basicdb").collection("inventory");
+      // finds all items in the collection
+      result = await collection.find({}).toArray();
+  
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    catch (e) {
+      console.dir(e)
+      error = e;
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+    
+    if (error === null) {
 
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-
-  res.json(movieTitles);
-
-});
-
+      res.json(result.map((value) => {
+        return value.title;
+      }))
+    }
+    else {
+      res.status(500).send("Failure");
+    }
+  })
+  .delete(async (req, res) => {
+    movieList = [];
+    res.sendStatus(200)
+  })
 
 app.get('/find', (req, res) => {
-  const searchTerm = req.query.contains || req.query.startsWith;
-  if (searchTerm) {
-    const filteredTitles = movieTitles.filter((title) =>
-      req.query.contains
-        ? title.toLowerCase().includes(searchTerm.toLowerCase())
-        : title.toLowerCase().startsWith(searchTerm.toLowerCase())
-    );
-    res.json(filteredTitles);
-  } else {
-    res.json(movieTitles);
+  console.log(req.query)
+  if (req.query.hasOwnProperty('contains')) {
+    res.json(movieList.filter((title) => title.toLowerCase().indexOf(req.query.contains.toLowerCase()) > -1))
   }
-});
+  else if (req.query.hasOwnProperty('startsWith')) {
+    res.json(movieList.filter((title) => title.toLowerCase().indexOf(req.query.startsWith.toLowerCase()) === 0))
+  }
+})
 
-app.delete('/all', (req, res) => {
-  movieTitles.length = 0;
-  res.send('All the Movie Titles removed from the array.');
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
